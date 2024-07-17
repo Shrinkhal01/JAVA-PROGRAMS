3a7f1a2a55d9d9c83337afdef6504644c967b43e
Shrinkhal01 <97280075+Shrinkhal01@users.noreply.github.com>
2024-07-17 10:21:31 +0530

    Update LINKEDLIST.java

--- LINKEDLIST.java
+++ LINKEDLIST.java
@@ -1,47 +1,40 @@
 import java.util.*;
-public class LINKEDLIST {
-    Node head;
+public class linkedlist {
+    static Node head;
     class Node{
         String data;
         Node next;
 
         Node(String data){
-            this.data=data;
+            this.data=data;//
             this.next=null;
         }
     }
 
+    //add- first , last
     public void addF(String data){
         Node newnode=new Node(data);
-        if(head==null){
+        if(head==null){ 
             head=newnode;
+            return;
         }
         newnode.next=head;
         head=newnode;
     }
 
+    //add - last
     public void addL(String data){
-        Node newnode=new Node(data);
+        Node newNode=new Node(data);
         if(head==null){
-            head=newnode;
+            head=newNode;
+            return;
         }
-        while(head.next!=null){
-            head=head.next;
+        Node curNode=head;
+        while(curNode.next!=null){
+            curNode=curNode.next;
         }
-        head.next=newnode;
+        curNode.next=newNode;
     }
-
-    public Node reverse(Node head){
-        if(head==null || head.next==null){
-            return head;
-        }
-
-        Node newhead=reverse(head.next);
-        head.next.next=head;
-        head.next=null;
-        return newhead;
-    }
-    
 
     public void printl(){
         Node curNode=head;
@@ -55,14 +48,68 @@ public class LINKEDLIST {
         System.out.println("NULL");
     }
 
+    public Node reverseI(Node head){
+
+        if(head==null||head.next==null){
+            return head;
+        }
+        Node newhead=reverseI(head.next);
+        head.next.next=head;
+        head.next=null;
+        return newhead;
+
+    }
+
+    
+
+
+    public static int length(Node a){
+        int i=0;
+        while(a!=null){
+            i++;
+            a=a.next;
+        }
+        return i;
+    }
+
+    public static void intersect(Node a, Node b) {
+        List<Integer> intersectingValues = new ArrayList<>();//new thing learnt
+    
+        while (a != null && b != null) {
+            if (a.data==b.data) {
+                intersectingValues.add(Integer.parseInt(a.data));
+            }
+            a = a.next;
+            b = b.next;
+        }
+    
+        if (intersectingValues.isEmpty()||intersectingValues==null) {
+            System.out.println("They don't intersect at any point in their entirety");
+        } else {
+            System.out.println("They intersect at the following nodes:");
+            for (Integer value : intersectingValues) {
+                System.out.println(value);
+            }
+        }
+    }
+    
 
     public static void main(String[] args) {
-        LINKEDLIST list=new LINKEDLIST();
+        linkedlist list=new linkedlist();
         list.addL("1");
         list.addL("2");
         list.addL("3");
+        list.addL("34");
+        list.addL("45");
         list.printl(); 
-        list.head=list.reverse(list.head);
-        list.printl();
+        linkedlist list22=new linkedlist();
+        list22.addL("31");
+        list22.addL("2");
+        list22.addL("10");
+        list22.addL("65");
+        list22.addL("45");
+        list22.printl();
+        
+        intersect(list.head,list22.head);
     }
 }
